4bfdc72e636fb52fd6a3056e7612ac20995e112e
Марьяна <@DESKTOP-8H8GHND>
2021-02-20 00:04:11 +0600

    Добавьте файлы проекта.

---
+++ lab_2/lab_2.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cmath>
+#define BORDER 16
+using namespace std;
+double scan_x_in_range(const double border);
+
+int main()
+{
+	const double border_x= 1;
+	const double x = scan_x_in_range(border_x);
+    double sum = 0, sign = 1;
+    for (int index = 0; index < BORDER; index ++){
+        sum = sign * (index + 1) * pow(x, index);
+        sign = -sign;
+    }
+    cout << "The value of the series at x = " << x << " is equal to " << sum;
+
+	return 0;
+}
+
+double scan_x_in_range(const double border)
+{
+	while (true) {
+		cout << "Enter a number less than " << border << " modulo: ";
+		double x = 0;
+		cin >> x;
+
+		if (cin.fail()) {
+			cout << "Entered is not a number.\n";
+			cin.clear();
+			std::cin.ignore(32767, '\n');
+		}
+		else {
+			if (x < fabs(border)) {
+				return x;
+			}
+			else {
+					cout << "The entered number is greater than or equal to " << border << " modulo\n";
+					cin.clear();
+			}
+		}
+	}
+}
+
